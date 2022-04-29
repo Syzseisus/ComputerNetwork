f0d614df4be609febbb9ff4268aed750b8b1d250
Syzseisus <83002480+Syzseisus@users.noreply.github.com>
2022-04-29 17:52:24 +0900

    Update my_client.py

--- MidTerm_Replace_HW/my_client.py
+++ MidTerm_Replace_HW/my_client.py
@@ -1,26 +1,48 @@
 '''
-This File creates client and send request.
-When user inputs {METHOD}, {PATH} and {REQUEST_BODY},
-then this file make HTTP-protocol-like request as:
+Send request as:
     {METHOD} / HTTP/1.1
-    Host: {IP}/{PATH}               # IP is hard coded as Variable.
+    Host: 127.0.0.1/{PATH}
     Content-Type: text/html
     Connection: keep-alive
     Content-Length: {len(REQUEST_BODY)}
 
     {REQUEST_BODY}
-
-and sendall request to connected server,
-receive RESPONSE and print {RESPONSE}
-
-If server sends response 100 CONTINUE of POST request,
-user has to re-input request body to successfully POST.
-
-You can END this inputing "EXIT" as {METHOD}
 '''
 from socket import *
 
-def request_formating(method, url, body, IP):
+
+### Variables: be aware of IP == server.IP / PORT == server.POST
+IP = '127.0.0.1'
+PORT = 8080
+
+
+### Test case
+test_case = [
+    # method, path, body
+    # 1. Correctly Act
+    ('HEAD', 'dummy.html', '', IP),
+    ('GET ', 'dummy.html', '', IP),
+    ('POST', 'create', '', IP),     # -> will resend "User-Affiliation=KMU"
+    ('POST', 'create', 'User-ID=K2022031&Grade=F', IP),
+    ('PUT' , 'update', 'Grade=A+&Major=Artificial Intelligence', IP),
+    ('GET' , 'dummy.html', '', IP),
+    # 2. Bad Act
+    # 1) Detected in `from_request_to_response`
+    ('HEAD', 'dummy.html', '', '1.1.1.1'),
+    ('GET' , 'foo.exe', '', IP),
+    ('HEAD', 'foo.html', '', IP),
+    ('FOO' , 'dummy.html', '', IP),
+    ('PUT' , 'foo', '', IP),
+    # 2) Detected in `POST`, `PUT`
+    ('POST', 'create', 'Grade=B', IP),
+    ('PUT' , 'create', '', IP),
+    # 3. END
+    ('END', '' , '', IP)
+]
+
+
+### Return Request
+def request_formatting(method, path, body, IP=IP):
     '''
     Use input {method}, {url}, {body}
     and variable {IP}
@@ -30,38 +52,32 @@ def request_formating(method, url, body, IP):
 
     return f'''\
 {method} / HTTP/1.1\r
-Host: {IP}/{url}\r
+Host: {IP}/{path}\r
 Content-Type: text/html\r
 Connection: keep-alive\r
 Content-Length: {len(body)}\r\n\n{body}'''
 
 
-### Variables: be aware of IP == server.IP / PORT == server.POST
-IP = '127.0.0.1'
-PORT = 8080
-
-
 ### Send request and Receive response until user input EXIT
+test = 0   # for test case
 while True:
+    print("#"*7, f"Test {test:02d}:\n {test_case[test]}")
+
     ## Create client socket and connect to server
     client_socket = socket(AF_INET, SOCK_STREAM)
     client_socket.connect( (IP, PORT) )
 
-    print('\n', "="*10, f" CONNECTED - {IP}:{PORT} ", "="*10, '\n', sep='')
-
-    ## User input
-    print(">>> ENTER REQUEST:\n")
-
-    method = input("input method in [HEAD, GET, POST, PUT, EXIT(end)]: ")
-    if method.upper() == "EXIT":        # Disconnect when user inputs EXIT
-        request = "END"
+    print('\n', "="*22, f" CONNECTED - {IP}:{PORT} ", "="*23, '\n', sep='')
+    
+    ## Make request
+    method, path, body, target = test_case[test]
+    if method == 'END': 
+        request = 'END'
         client_socket.sendall(request.encode('utf-8'))
         break
     else:
-        url  = input("input html url or create/update for POST/PUT: ")
-        body = input("input request details:")
-        body = body if body else ''
-        request = request_formating(method, url, body, IP)
+        request = request_formatting(method, path, body, target)
+        test += 1  # for test case
 
     ## Send request
     print("\n>>> Send Request ...")
@@ -72,11 +88,12 @@ while True:
     response = client_socket.recv(65535).decode('utf-8')
     
     # If receive POST 100 CONTINUE
-    if 'What do you want to' in response.split('\n')[-1]:
+    if 'What do you want to' in response.split('\n')[-2]:
         while True:
-            body = input("input request details again:")
+            # body = input("input request details again:")
+            body = 'User-Affiliation=KMU'
             if body:
-                request = request_formating(method, url, body, IP)
+                request = request_formatting(method, path, body, IP)
                 print("\n>>> Send content wanted to be POST ...")
                 client_socket.sendall(request.encode('utf-8'))
                 print("\n>>> Receive Response ...\n")
@@ -87,10 +104,9 @@ while True:
     print(response)
 
     ## End
-    print("="*20, " ENDED ", "="*21, '\n', sep='')
+    print("="*33, " ENDED ", "="*33, '\n\n', sep='')
     client_socket.close()
 
-    
 ### Close client when break
 client_socket.close()
 print("\nClient is successfully closed")
